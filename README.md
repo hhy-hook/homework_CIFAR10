08363399ca5222e5bb0f485b119455a37c4d2044
hhy-hook <798437955@qq.com>
2022-09-19 16:09:00 +0800

    first push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homework_CIFAR10
+## 1.运行环境搭建
+cuda11.3 + torch 10.0
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('hello')
+import torch
+print(torch.cuda.device_count())
+print(torch.version.cuda)
+print(torch.backends.cudnn.enabled)
